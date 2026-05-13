e8eeaeae6e7379793dea03b26043e660b635c3a2
Алексей Тихоненко <a.tikhonenko@crpt.ru>
2026-05-13 19:49:20 +0500

    RLS-1

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,8 @@
-export changed=$(npx lerna changed --all --include-merged-tags)
-if [ -z "$changed" ]; then
-  echo 'No changed packages found';
-  exit;
-fi
+# export changed=$(yarn lerna changed --all --include-merged-tags)
+# if [ -z "$changed" ]; then
+#   echo 'No changed packages found';
+#   exit;
+# fi
 
 export jira_issue_id=$(grep -o -E "(RLS+-[0-9]+)" <<< $(git log -1 --oneline))
 if [ -z "$jira_issue_id" ]; then
@@ -10,13 +10,13 @@ if [ -z "$jira_issue_id" ]; then
   exit;
 fi
 
-git tag -d $(git tag -l) >/dev/null && git fetch --tags --quiet
-git fetch origin main --quiet
+# git tag -d $(git tag -l) >/dev/null && git fetch --tags --quiet
+# git fetch origin main --quiet
 
-tags=$(git tag -l | grep $jira_issue_id)
-if [ ! -z "$tags" ]; then
-  xargs -n 1 git push -d origin <<< $tags
-  xargs -n 1 git tag -d <<< $tags
-fi
+# tags=$(git tag -l | grep $jira_issue_id)
+# if [ ! -z "$tags" ]; then
+#   xargs -n 1 git push -d origin <<< $tags
+#   xargs -n 1 git tag -d <<< $tags
+# fi
 
-npx lerna version --conventional-commits --conventional-graduate --include-merged-tags --yes --message "chore: publish versions $jira_issue_id"
+yarn publish:versions --message "chore: publish versions $jira_issue_id"
