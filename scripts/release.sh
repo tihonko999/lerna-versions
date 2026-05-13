2c70ff29ba9a32a386e36c359f02df7ab522d590
Алексей Тихоненко <a.tikhonenko@crpt.ru>
2026-05-13 19:58:59 +0500

    release.sh очистка от ненужного

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,22 +1,8 @@
-# export changed=$(yarn lerna changed --all --include-merged-tags)
-# if [ -z "$changed" ]; then
-#   echo 'No changed packages found';
-#   exit;
-# fi
-
 export jira_issue_id=$(grep -o -E "(RLS+-[0-9]+)" <<< $(git log -1 --oneline))
+
 if [ -z "$jira_issue_id" ]; then
   echo 'No jira issue id found';
   exit;
 fi
-
-# git tag -d $(git tag -l) >/dev/null && git fetch --tags --quiet
-# git fetch origin main --quiet
-
-# tags=$(git tag -l | grep $jira_issue_id)
-# if [ ! -z "$tags" ]; then
-#   xargs -n 1 git push -d origin <<< $tags
-#   xargs -n 1 git tag -d <<< $tags
-# fi
 
 yarn publish:versions --message "chore: publish versions $jira_issue_id"
