06d985e84c56ed845c4a7d4b8044bf7caf86de6e
Алексей Тихоненко <a.tikhonenko@crpt.ru>
2026-05-13 20:05:20 +0500

    release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,8 @@
+# скрипт по последнему коммиту получает jira_issue_id
+# при отсутствии jira_issue_id скрипт прерывается
+# запускается lerna version:
+# поднимаются версии измененных пакетов, создается коммит, создаются git теги для каждого поднятого пакета, выполняетя пуш в gitlab
+
 export jira_issue_id=$(grep -o -E "(RLS+-[0-9]+)" <<< $(git log -1 --oneline))
 
 if [ -z "$jira_issue_id" ]; then
@@ -5,4 +10,4 @@ if [ -z "$jira_issue_id" ]; then
   exit;
 fi
 
-yarn publish:versions --message "chore: publish versions $jira_issue_id"
+yarn lerna version --message "chore: publish versions $jira_issue_id"
